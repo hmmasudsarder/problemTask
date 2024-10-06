d22d2ea1b22359481add4f9a052abe22229255e8
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-10-06 10:54:16 +0600

    complated all problem solved

--- some-problem.js
+++ some-problem.js
@@ -22,20 +22,20 @@
 
 // 02 Write a function that takes an array and returns the maximum number.
 
-function maxNumber(arr) {
-    let maxNum = arr[0];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i]>maxNum) {
-            maxNum=arr[i]
-        }
-    }
-    // const masNum = Math.max(maxNum)
-    return maxNum
-}
+// function maxNumber(arr) {
+//     let maxNum = arr[0];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i]>maxNum) {
+//             maxNum=arr[i]
+//         }
+//     }
+//     // const masNum = Math.max(maxNum)
+//     return maxNum
+// }
 
-const numbers = [12473, 321, 21, 12, 329876]
-const result = maxNumber(numbers)
-console.log(result);
+// const numbers = [12473, 321, 21, 12, 329876]
+// const result = maxNumber(numbers)
+// console.log(result);
 
 
 //03 Write a function that takes an array and returns a new array with the elements in reverse order.
@@ -60,7 +60,23 @@ console.log(result);
 // console.log(removeSpecificElement(number, 3));
 
 
+//05 Write a function that counts how many times a specific value appears in an array.
 
+// function manyTime(arr, exists) {
+//     const count = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         const element = arr[i];
+//         if (element === exists) {
+//             count.push(element)
+//         }
+//     }
+//     const lengthCount = count.length
+//     return { count, lengthCount }
+// }
+
+// const numbers = [1, 2, 1, 3, 2, 3, 4, 5, 6, 6, 6, 6, 7, 8, 9]
+// const res = manyTime(numbers, 6)
+// console.log(res);
 
 // 06 Write a function that removes duplicate values from an array.
 // const dublicate = ["masud", "masodur", "masud", "somun"];
@@ -79,6 +95,37 @@ console.log(result);
 // const result = dublicateF(dublicate)
 // console.log(result);
 
+// 07 Write a function that merges two arrays and returns the result.
+
+function combail(arr, arr1) {
+    const result = [...arr, ...arr1];
+    return result
+}
+
+// const arr = [3, 23, 4, 5,];
+// const arr1 = [43, 5, 3, 6, 9]
+// let resu = combail(arr, arr1);
+// console.log(resu);
+
+//08 Write a function that returns a new array containing only the elements that are present in both arrays.
+
+function containings(arr1, arr2){
+    const newArry = [];
+    for (const element of arr1) {
+        for (const element2 of arr2) {
+            if (element === element2) {
+                newArry.push(element)
+            }
+        }
+    }
+    return newArry
+}
+
+const arr = [1, 2, 3, 4, 5,];
+const arr1 = [3, 4, 5, 6, 9]
+
+// const res = containings(arr, arr1)
+// console.log(res);
 
 //09 Write a function that removes a specific element from an array.
 // function removeSpecificElement(numbers, removenum) {
@@ -88,3 +135,25 @@ console.log(result);
 // const number = [1, 2, 3];
 
 // console.log(removeSpecificElement(number, 2));
+
+
+
+
+function sumArray(array1, array2){
+    let arrapu = [];
+    for (let i = 0; i < array1.length; i++) {
+            for (let index = 0; index < array1.length; index++) {
+                if (array1[i]+array1[index]===array2[0]) {
+                    arrapu.push(i)
+                    // arrapu.push(index)
+                
+                }
+            }        
+    }
+    return arrapu
+}
+
+const array1 = [4, 5, 2, 8, 1, 2];
+const array2 = [9];
+const res = sumArray(array1, array2)
+console.log(res);
